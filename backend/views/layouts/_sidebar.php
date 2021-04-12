64c51f0be2bb3b249e48d11aca232ab9669c9c76
JAYAROSHINI2112 <roshinicandy2112@gmail.com>
2021-04-12 21:32:30 +0530

    first commit

---
+++ backend/views/layouts/_sidebar.php
@@ -0,0 +1,18 @@
+
+<aside class="shadow" style=" min-width:200px;">
+<?php echo \yii\bootstrap4\Nav::widget([
+    'options' =>[
+    'class' => 'd-flex flex-column nav-pills'
+    ],
+    'items' => [
+        [
+            'label' => 'Dashboard',
+            'url'  => ['/site/index']
+        ],
+        [
+            'label' => 'Videos',
+            'url'  => ['/video/index']
+        ]
+        ]
+]) ?>
+</aside>
